7dbd9e50d5c8731446d33e459daa957cda95c88f
Rizki Cahya <rizki.cahya03@gmail.com>
2025-08-06 00:04:59 +0700

    feat: change font in login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import TaskPage from "./pages/TaskPage";
+import './index.css';
 
 function App() {
     return (
