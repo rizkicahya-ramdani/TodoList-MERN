12938ea2f2c5b3eaa2a25d0dedd5fe63e6e276fb
Rizki Cahya <rizki.cahya03@gmail.com>
2025-08-07 21:55:18 +0700

    some error/bug

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import userRoutes from "./routes/userRoutes";
-import taskRoutes from "./routes/taskRoutes";
+import userRoutes from './routes/userRoutes.ts';
+import taskRoutes from './routes/taskRoutes.ts';
 
-const app: Express = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -21,12 +21,8 @@ mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.error(err));
 
-// Rute API
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
-const PORT: number = Number(process.env.PORT) || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const PORT = Number(process.env.PORT) || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
